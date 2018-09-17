b8ea3a77e7505c3b01c44bf62433a80c18af09f9
Yamam kurdi <yamamkurdi0@gmail.com>
2018-09-17 11:47:26 +0200

    ex5

---
+++ Basic5.js
@@ -0,0 +1,9 @@
+function box(width,height,depth){
+	var area = width * height 
+	var volume = width * height * depth
+	document.write("area = " + area)
+	document.write("<br>volume = " + volume)
+}
+
+	
+	box (7,2,5)

d8801ca06c1231febb1ed30841799d9bffb9910b
Yamam kurdi <yamamkurdi0@gmail.com>
2018-09-17 10:04:03 +0200

    Basic-ex1

---
+++ java.js
@@ -0,0 +1,5 @@
+function crystalGazer(jobTitle,location,partnerName,NOC){
+	document.write("You will be a "+jobTitle+" in "+location+" and married to "+partnerName+" with "+NOC);
+}
+
+crystalGazer("Developer","Vienna","Martina",2);
